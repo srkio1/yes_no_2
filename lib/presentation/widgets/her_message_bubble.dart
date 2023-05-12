cef298546ee5b79884e778c47dd3b072f258720c
Carlos Daniel RS <ingdanielruizsaucedo@gmail.com>
2023-05-12 10:48:29 -0400

    Funcionando despues del video 9

--- lib/presentation/widgets/her_message_bubble.dart
+++ lib/presentation/widgets/her_message_bubble.dart
@@ -40,6 +40,14 @@ class _ImageBubble extends StatelessWidget {
         width: size.width * 0.7,
         height: 150,
         fit: BoxFit.cover,
+        loadingBuilder: (context, child, loadingProgress) {
+          if (loadingProgress == null) return child;
+          return Container(
+            width: size.width * 0.2,
+            height: 150,
+            child: Text("me estan mandando una imagen"),
+          );
+        },
       ),
     );
   }
