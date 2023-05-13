1527fe5e30de00a35b7cd504d6f96a0d76ed74f8
Carlos Daniel RS <ingdanielruizsaucedo@gmail.com>
2023-05-13 01:35:22 -0400

    final de la seccion 5 yes o no app

--- lib/presentation/widgets/shared/message_field_box.dart
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -3,27 +3,42 @@ import 'package:flutter/material.dart';
 class MessageFieldBox extends StatelessWidget {
   MessageFieldBox({super.key});
 
-  
-
   @override
   Widget build(BuildContext context) {
+    final textController = TextEditingController();
+    final focusNode = FocusNode();
     final outlineInputBorder = UnderlineInputBorder(
-    borderSide: const BorderSide(color: Colors.transparent),
-    borderRadius: BorderRadius.circular(40),
-  );
-  final inputdecoration = InputDecoration(
-        enabledBorder: outlineInputBorder,
-        focusedBorder: outlineInputBorder,
-        filled: true,
-        suffixIcon: IconButton(
-          icon: const Icon(
-            Icons.send_and_archive_outlined,
-          ),
-          onPressed: () {},
+      borderSide: const BorderSide(color: Colors.transparent),
+      borderRadius: BorderRadius.circular(40),
+    );
+    final inputdecoration = InputDecoration(
+      hintText: "Introduce tu mensaje?",
+      enabledBorder: outlineInputBorder,
+      focusedBorder: outlineInputBorder,
+      filled: true,
+      suffixIcon: IconButton(
+        icon: const Icon(
+          Icons.send_and_archive_outlined,
         ),
-      );
+        onPressed: () {
+          //para obtener el valor de la caja de texto
+          final textValue = textController.value.text;
+          print("este es el valor de la caja de texto $textValue");
+        },
+      ),
+    );
     return TextFormField(
+      onTapOutside: (event) {
+        focusNode.unfocus();
+      },
+      focusNode: focusNode,
       decoration: inputdecoration,
+      controller: textController,
+      onFieldSubmitted: (value) {
+        textController.clear();
+        //para conservar el focus en el campo
+        focusNode.requestFocus();
+      },
     );
   }
 }
