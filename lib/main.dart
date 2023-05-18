1bfd0672fd5073dcb55aed8345dfdae41f29edc8
Carlos Daniel RS <ingdanielruizsaucedo@gmail.com>
2023-05-18 11:14:24 -0400

    final video 7

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:yes_no_2/config/theme/app_theme.dart';
+import 'package:yes_no_2/presentation/providers/chat_providers.dart';
 import 'package:yes_no_2/presentation/screens/chat/chat_screen.dart';
 
 void main() => runApp(MyApp());
@@ -9,10 +11,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: AppTheme(selectColor: 1).theme(),
-      title: 'Material App',
-      home: ChatScreen(),
+    return MultiProvider(
+      providers: [ChangeNotifierProvider(create: (_) => ChatProvider(),) ],
+      child: MaterialApp(
+        theme: AppTheme(selectColor: 1).theme(),
+        title: 'Material App',
+        home: ChatScreen(),
+      ),
     );
   }
 }
